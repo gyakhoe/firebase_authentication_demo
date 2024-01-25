533a61ecb2a1684a812a6b35e4a114a3db250400
Gyague Sonam <gyakhoe@gmail.com>
2024-01-25 18:18:50 -0500

    Initial commit

---
+++ android/app/src/main/kotlin/com/gyague/sonam/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.gyague.sonam.firebase_authentication_demo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:firebase_authentication_demo/app/app.dart';
+import 'package:firebase_authentication_demo/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
